2b4c9affd915a7ced3204e15cb67c7befd457c82
Deanna Sharma <deanna.sharma@doza.consulting>
2025-11-06 16:01:49 +0000

    fix: update response

--- src/functions/cancellations-siri-sx-downloader/index.ts
+++ src/functions/cancellations-siri-sx-downloader/index.ts
@@ -64,7 +64,7 @@ export const handler: APIGatewayProxyHandler = async (event, context): Promise<A
         } = process.env;
 
         if (stage === "prod") {
-            createHttpValidationErrorResponse(["Invalid request"]);
+            return createHttpValidationErrorResponse(["Invalid request"]);
         }
 
         if (!bucketName) {
